7652a6f76253b311887053e70a6b0ae60fae06e4
leeroy999 <leeroy.whatsup@rocketmail.com>
2022-09-03 20:57:46 +0200

    add camera

---
+++ src/components/CameraModal.jsx
@@ -0,0 +1,31 @@
+import { Modal, Paper } from "@mui/material";
+import Camera from 'react-html5-camera-photo';
+import "react-html5-camera-photo/build/css/index.css";
+
+const CameraModal = (props) => {
+  console.log(props);
+  const { isOpen, handleClose, addIngredient } = props;
+
+  const handleTakePhoto = (dataUri) => {
+    console.log(dataUri);
+    handleClose();
+  }
+  
+  return (
+    <Modal
+      open={isOpen}
+      onClose={handleClose}
+      style={{display:'flex', alignItems:'center', justifyContent:'center'}}
+    >
+      <Paper style={{padding: '20px'}}>
+        <Camera
+          onTakePhoto={handleTakePhoto}
+          idealFacingMode="environment"
+          isImageMirror={false}
+        />
+      </Paper>
+    </Modal>
+  )
+};
+
+export default CameraModal;
--- src/components/CookingForm.jsx
+++ src/components/CookingForm.jsx
@@ -1,9 +1,25 @@
-import { Search } from '@mui/icons-material';
-import { FormControl, InputBase, IconButton, Paper } from '@mui/material';
-import { Box } from '@mui/system';
+import { CameraAlt, Search } from '@mui/icons-material';
+import { FormControl, InputBase, IconButton, Paper, Box } from '@mui/material';
+import { useState } from 'react';
+import CameraModal from './CameraModal';
 import './CookingForm.css';
 
 const CookingForm = () => {
+  const [ingredients, setIngredients] = useState('');
+  const [openCamera, setOpenCamera] = useState(false);
+
+  const handleIngredients = (e) => {
+    setIngredients(e.target.value)
+  };
+
+  const addIngredient = (aiIngredient) => {
+    setIngredients(ingredients + aiIngredient);
+  };
+
+  const handleCloseModal = () => {
+    setOpenCamera(false);
+  };
+
   return (
     <Box className="App">
       <header className="App-header">
@@ -13,16 +29,27 @@ const CookingForm = () => {
             component="form"
             sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
           >
+            <Search />
             <InputBase
               sx={{ ml: 1, flex: 1}}
+              value={ingredients}
               placeholder="Ingredient1, Ingredient2, ..."
+              onChange={handleIngredients}
             />
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-              <Search />
+            <IconButton
+              type="button"
+              sx={{ p: '10px' }}
+              aria-label="search"
+              onClick={() => setOpenCamera(true)}
+            >
+              <CameraAlt />
             </IconButton>
           </Paper>
         </FormControl>
       </header>
+      <Box sx={{alignItems: 'center'}}>
+        <CameraModal isOpen={openCamera} handleClose={handleCloseModal} addIngredient={addIngredient} />
+      </Box>
     </Box>
   );
 }
