a0956e21d299625ba79704f8d4ea7fb1abddc2c2
leeroy999 <leeroy.whatsup@rocketmail.com>
2022-09-03 16:48:30 +0200

    refactor structure

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Cooking Recipes
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
